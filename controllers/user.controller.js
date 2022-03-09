66de5879af4994f83bf959b51a82ba0138e6fddf
Aurelien <aurelienmuset@gmail.com>
2022-03-09 21:33:18 +0100

    crud user.controllers

---
+++ controllers/user.controller.js
@@ -0,0 +1,41 @@
+const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
+
+// select all users (en enlevant le password de la requête)
+module.exports.getAllUsers = async (req, res) => {
+  const users = await UserModel.find().select("-password");
+  res.status(200).json(users);
+};
+
+// select un user par l'ID (en enlevant le password de la requête)
+module.exports.userInfo = (req, res) => {
+  console.log(req.params);
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  UserModel.findById(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else console.log("ID unknwown : " + err);
+  }).select("-password");
+};
+
+module.exports.updateUser = async (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID invalid : " + req.params.id);
+
+  try {
+    await UserModel.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $set: {
+          bio: req.body.bio,
+        },
+      },
+      { new: true, upsert: true, setDefaultsOnInsert: true }
+    )
+      .then((docs) => res.send(docs))
+      .catch((err) => res.status(500).send({ message: err }));
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
