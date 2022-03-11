5f044987c56a92726e70003318c3e21a270a9924
Aurelien <aurelienmuset@gmail.com>
2022-03-11 20:02:54 +0100

    post controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,11 @@
 const UserModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const { signUpErrors } = require("../utils/errors.utils");
 
+const maxAge = 3 * 24 * 60 * 60 * 1000;
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_SECRET, {
-    expiresIn: 3 * 24 * 60 * 60 * 1000,
+    expiresIn: maxAge,
   });
 };
 
@@ -16,7 +18,10 @@ module.exports.signUp = async (req, res) => {
     res.status(201).json({ user: user._id });
   } catch (err) {
     // const errors = signUpErrors(err);
-    res.status(200).send({ err });
+
+    const errors = signUpErrors(err);
+
+    res.status(200).send({ errors });
   }
 };
 
@@ -26,7 +31,13 @@ module.exports.signIn = async (req, res) => {
   try {
     const user = await UserModel.login(email, password);
     const token = createToken(user._id);
-  } catch (err) {}
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(200).json(err);
+  }
 };
-
-module.exports.logout = async (req, res) => {};
+module.exports.logout = (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
+};
