f3095add905c4b26c73fe3cd15763f18ca08ea12
Aurelien <aurelienmuset@gmail.com>
2022-03-11 20:02:43 +0100

    post controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
 const userController = require("../controllers/user.controller");
-
+const uploadController = require("../controllers/upload.controller");
 // auth
 router.post("/register", authController.signUp);
 router.post("/login", authController.signIn);
@@ -14,5 +14,8 @@ router.put("/:id", userController.updateUser);
 router.delete("/:id", userController.deleteUser);
 router.patch("/follow/:id", userController.follow);
 router.patch("/unfollow/:id", userController.unfollow);
+
+//upload
+router.post('/upload',  , uploadController.uploadProfil)
 
 module.exports = router;
